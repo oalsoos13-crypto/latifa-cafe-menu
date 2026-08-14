83bc031904ce482370e5c8e8e1eb98bca5bedde8
oalsoos13-crypto <oalsoos13@gmail.com>
2026-08-14 19:12:32 +0300

    Latifa Cafe PWA menu

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CORE = 'latifa-core-v3';
+const RUNTIME = 'latifa-runtime-v3';
+const PRECACHE = ['./index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CORE).then(c => c.addAll(PRECACHE)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CORE && k !== RUNTIME).map(k => caches.delete(k))
+    )).then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request, {ignoreSearch: false}).then(hit => {
+      if (hit) return hit;
+      return fetch(e.request).then(res => {
+        if (res && (res.ok || res.type === 'opaque')) {
+          const copy = res.clone();
+          caches.open(RUNTIME).then(c => c.put(e.request, copy));
+        }
+        return res;
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
+});
